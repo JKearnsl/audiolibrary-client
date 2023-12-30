78febd34285c9315572bf0876c3efc91d088aefb
JKearnsl <boyarshinovn@gmail.com>
2023-12-31 01:02:04 +0300

    Обновлен уровень инкапсуляции layout

--- src/audiolibrary/views/widgets/dialog.py
+++ src/audiolibrary/views/widgets/dialog.py
@@ -1,6 +1,7 @@
 from PyQt6 import QtCore, QtGui
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLayout, QHBoxLayout, QWidget, QGraphicsDropShadowEffect
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLayout, QHBoxLayout, QWidget, QGraphicsDropShadowEffect, \
+    QLabel
 
 
 class Dialog(QDialog):
@@ -41,12 +42,12 @@ class Dialog(QDialog):
         general_layout.setContentsMargins(0, 0, 0, 0)
         general_layout.setSpacing(0)
 
-        header_layout = QVBoxLayout()
+        header_layout = QHBoxLayout()
         header_layout.setContentsMargins(0, 0, 0, 0)
-        header_layout.setSpacing(0)
-        header_layout.setAlignment(Qt.AlignmentFlag.AlignRight)
+        # header_layout.addStretch(1)
         general_layout.addLayout(header_layout)
 
+        # Close button
         exit_button = QPushButton(self)
         exit_button.setFixedSize(30, 30)
         exit_button.setText("×")
@@ -78,14 +79,72 @@ class Dialog(QDialog):
             color=QtGui.QColor(0, 0, 0, 50),
             offset=QtCore.QPointF(0, 0)
         ))
-
         exit_button.clicked.connect(self.close)
+
+        # Title
+        title_widget = QWidget(self)
+        title_widget.hide()
+        title_widget.setFixedSize(
+            self.width() - exit_button.width() - (30 * self.width() // 100),
+            30
+        )
+        title_widget.setStyleSheet("""
+            QWidget {
+                background-color: $BG2;
+            
+                border-top-left-radius: 10px;
+                border-bottom-right-radius: 10px;
+                color: $TEXT_HEADER;
+                font-size: 26px;
+                font-weight: bold;
+            }
+        """.replace(
+            "$BG2", background_close_btn
+        ).replace(
+            "$TEXT_HEADER", text_color_close_btn
+        ))
+
+        title_layout = QHBoxLayout(title_widget)
+        title_layout.setContentsMargins(10, 0, 0, 0)
+        title_layout.setSpacing(0)
+
+        title_label = QLabel(self)
+        title_label.setObjectName("title_label")
+        title_label.setStyleSheet("""
+            QLabel#title_label {
+                color: $TEXT_HEADER;
+                font-size: 12px;
+                font-weight: bold;
+            }
+        """.replace(
+            "$TEXT_HEADER", text_color_close_btn
+        ))
+        self._title_label = title_label
+        title_layout.addWidget(title_label)
+        self._title_widget = title_widget
+        title_widget.setGraphicsEffect(QGraphicsDropShadowEffect(
+            blurRadius=10,
+            color=QtGui.QColor(0, 0, 0, 50),
+            offset=QtCore.QPointF(0, 0)
+        ))
+
+        header_layout.addWidget(title_widget)
+        header_layout.addStretch(1)
         header_layout.addWidget(exit_button)
 
-        self.__layout = QVBoxLayout()
-        self.__layout.setContentsMargins(0, 0, 0, 0)
-        self.__layout.setSpacing(0)
-        general_layout.addLayout(self.__layout)
+        self._layout = QVBoxLayout()
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self._layout.setSpacing(0)
+        general_layout.addLayout(self._layout)
+        general_layout.addStretch(1)
 
     def layout(self) -> QLayout | None:
-        return self.__layout
+        return self._layout
+
+    def set_title(self, title: str = None):
+        if title:
+            self._title_label.setText(title)
+            self._title_widget.show()
+        else:
+            self._title_widget.hide()
+        self.setWindowTitle(title)
