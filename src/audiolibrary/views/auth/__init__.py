ec25875ad444b5f7f5d7c5d6b50f4f5e928c1fc8
JKearnsl <boyarshinovn@gmail.com>
2023-12-30 17:48:36 +0300

    Переработан дизайн страницы входа, добавлены анимации

--- src/audiolibrary/views/auth/__init__.py
+++ src/audiolibrary/views/auth/__init__.py
@@ -1,43 +1,50 @@
-from PyQt6 import QtWidgets
+from typing import Callable
+
+from PyQt6 import QtWidgets, QtGui, QtCore
+from PyQt6.QtCore import QPropertyAnimation, QEasingCurve
+from PyQt6.QtWidgets import QGraphicsDropShadowEffect
 
 from audiolibrary.api_service import ErrorType
 from audiolibrary.utils.observer import DObserver
 from audiolibrary.utils.ts_meta import TSMeta
 from audiolibrary.views.auth.static_ui import UiAuthWindow
+from audiolibrary.views.widgets import WidgetsFactory
 
 
 class AuthView(QtWidgets.QWidget, DObserver, metaclass=TSMeta):
 
-    def __init__(self, controller, model, parent=None):
+    def __init__(self, controller, model, widgets_factory: WidgetsFactory, parent=None):
         super().__init__(parent)
+        self.animation_buffer = []
         self.controller = controller
         self.model = model
+        self.widgets_factory = widgets_factory
 
         self.ui = UiAuthWindow()
-        self.ui.setup_ui(self)
+        self.ui.setup_ui(self, widgets_factory)
         self.is_signin = True
 
         # Регистрация представлений
         self.model.add_observer(self)
 
         # События
-        self.ui.switchAuthStateLabel.mousePressEvent = lambda event: self.switch_auth_state(not self.is_signin)
+        self.ui.poster_button.mousePressEvent = lambda event: self.switch_auth_state(not self.is_signin)
         self.ui.signin_button.clicked.connect(self.signin_clicked)
         self.ui.signup_button.clicked.connect(self.signup_clicked)
 
     def model_changed(self):
-        self.ui.authResponseLabel.clear()
-        self.ui.authResponseLabel.hide()
+        self.ui.response_label.clear()
+        self.ui.response_label.hide()
 
     def model_loaded(self):
-        self.switch_auth_state(self.is_signin)
+        self.signin_form()
 
     def error_handler(self, error):
-        self.ui.authResponseLabel.show()
+        self.ui.response_label.show()
         if error.type is ErrorType.MESSAGE:
-            self.ui.authResponseLabel.setText(error.content)
+            self.ui.response_label.setText(error.content)
         else:
-            self.ui.authResponseLabel.setText('\n'.join(
+            self.ui.response_label.setText('\n'.join(
                 [
                     f"{el.field}: {el.message}" for el in error.content
                 ]
@@ -55,19 +62,44 @@ class AuthView(QtWidgets.QWidget, DObserver, metaclass=TSMeta):
         self.controller.signup(login, password, repeat_password)
 
     def signin_form(self):
-        self.ui.authTitleLabel.setText("Авторизация")
-        self.ui.switchAuthStateLabel.setText("Регистрация")
+        self.ui.title_label.setText("Вход")
+        self.ui.poster_button.setText("Регистрация")
+        self.ui.poster_header.setText("Привет, друг!")
+        self.ui.poster_text.setText(
+            "Зарегистрируйтесь, указав свои данные, чтобы использовать все возможности приложения"
+        )
 
         self.ui.password_line_edit.clear()
         self.ui.repeat_password_line_edit.clear()
         self.ui.signup_button.hide()
         self.ui.repeat_password_line_edit.hide()
-
         self.ui.signin_button.show()
 
+        self.ui.poster_panel.setStyleSheet(
+            self.ui.poster_panel.styleSheet() + """
+                QFrame {
+                    border-top-right-radius: 0px;
+                    border-bottom-right-radius: 0px;
+                    border-top-left-radius: 50px;
+                    border-bottom-left-radius: 50px;
+                }
+            """
+        )
+        self.ui.poster_panel.setGraphicsEffect(
+            QGraphicsDropShadowEffect(
+                blurRadius=10,
+                color=QtGui.QColor(0, 0, 0, 60),
+                offset=QtCore.QPointF(-1, 0)
+            )
+        )
+
     def signup_form(self):
-        self.ui.authTitleLabel.setText("Регистрация")
-        self.ui.switchAuthStateLabel.setText("Авторизация")
+        self.ui.title_label.setText("Регистрация")
+        self.ui.poster_button.setText("Вход")
+        self.ui.poster_header.setText("Добро пожаловать!")
+        self.ui.poster_text.setText(
+            "Введите свои данные, чтобы использовать все возможности приложения"
+        )
 
         self.ui.password_line_edit.clear()
         self.ui.repeat_password_line_edit.clear()
@@ -76,9 +108,69 @@ class AuthView(QtWidgets.QWidget, DObserver, metaclass=TSMeta):
         self.ui.repeat_password_line_edit.show()
         self.ui.signup_button.show()
 
-    def switch_auth_state(self, is_signin: bool):
+        self.ui.poster_panel.setStyleSheet(
+            self.ui.poster_panel.styleSheet() + """
+                QFrame {
+                    border-top-right-radius: 50px;
+                    border-bottom-right-radius: 50px;
+                    border-top-left-radius: 0px;
+                    border-bottom-left-radius: 0px;
+                }
+            """
+        )
+        self.ui.poster_panel.setGraphicsEffect(
+            QGraphicsDropShadowEffect(
+                blurRadius=10,
+                color=QtGui.QColor(0, 0, 0, 60),
+                offset=QtCore.QPointF(1, 0)
+            )
+        )
+
+    def switch_auth_state(self, is_signin):
+        for anim in self.animation_buffer:
+            if anim.state() == QPropertyAnimation.State.Running:
+                return
+
         self.is_signin = is_signin
-        if is_signin:
-            self.signin_form()
+
+        self.animate_widgets_switch(
+            duration=1000,
+            exec_finished=lambda: self.signin_form() if is_signin else self.signup_form()
+        )
+
+    def animate_widgets_switch(self, duration: int, exec_finished: Callable = None):
+        cover_widget = self.ui.poster_panel
+
+        # Анимация для cover_widget
+        animation_cover = QPropertyAnimation(cover_widget, b"geometry")
+        animation_cover.setDuration(duration)
+        animation_cover.setEasingCurve(QEasingCurve.Type.OutCubic)
+        animation_cover.setStartValue(cover_widget.geometry())
+
+        if self.ui.horizontal_layout.itemAt(0).widget() == cover_widget:
+            animation_cover.setEndValue(
+                QtCore.QRect(
+                    cover_widget.x(), cover_widget.y(), cover_widget.width() + 100, cover_widget.height()
+                )
+            )
         else:
-            self.signup_form()
+            animation_cover.setEndValue(
+                QtCore.QRect(
+                    cover_widget.x() - 100, cover_widget.y(), cover_widget.width() + 100, cover_widget.height()
+                )
+            )
+
+        self.animation_buffer.append(animation_cover)
+        animation_cover.start()
+
+        # Обработчик события `finished` для анимации
+        animation_cover.finished.connect(self.switch_widgets)
+        animation_cover.finished.connect(lambda: exec_finished() if exec_finished else None)
+        animation_cover.finished.connect(lambda: self.animation_buffer.remove(animation_cover))
+
+    def switch_widgets(self):
+        widgets = [self.ui.horizontal_layout.itemAt(i).widget() for i in range(self.ui.horizontal_layout.count())]
+
+        for widget in reversed(widgets):
+            self.ui.horizontal_layout.removeWidget(widget)
+            self.ui.horizontal_layout.addWidget(widget)
--- src/audiolibrary/views/auth/static_ui.py
+++ src/audiolibrary/views/auth/static_ui.py
@@ -1,288 +1,189 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QGraphicsDropShadowEffect
 
+from audiolibrary.views.widgets import WidgetsFactory
+
 
 class UiAuthWindow(object):
-    def setup_ui(self, window: QtWidgets.QWidget):
+    def setup_ui(self, window: QtWidgets.QWidget, widgets_factory: WidgetsFactory):
         window.setObjectName("window")
-        window.resize(800, 600)
+        window.resize(800, 500)
         window.setMinimumWidth(500)
         window.setMinimumHeight(400)
         window.setMaximumWidth(1920)
         window.setMaximumHeight(1080)
         window.setStyleSheet("""
             QLabel {
-                color:  #1a3f13;
+                color: $TEXT_PRIMARY;
                 font-weight: bold;
                 font-size: 14px;
             }
             
             QWidget#window {
-                background-color: rgb(220, 226, 218);
+                background-color: $BG1;
                 border: none;
             }
-        """)  # old background #dce29f
-        self.horizontalLayout = QtWidgets.QHBoxLayout(window)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        """.replace(
+            "$BG1", widgets_factory.theme.first_background
+        ).replace(
+            "$TEXT_PRIMARY", widgets_factory.theme.text_primary
+        ))
+        horizontal_layout = QtWidgets.QHBoxLayout(window)
+        horizontal_layout.setObjectName("horizontal_layout")
+        horizontal_layout.setContentsMargins(0, 0, 0, 0)
+        horizontal_layout.setSpacing(0)
+        self.horizontal_layout = horizontal_layout
 
-        # Основной макет
-        self.leftVerticalLayout = QtWidgets.QVBoxLayout()
-        self.leftVerticalLayout.setObjectName("leftVerticalLayout")
-        self.leftVerticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.leftVerticalLayout.addItem(
-            QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Policy.Expanding,
-                                  QtWidgets.QSizePolicy.Policy.Expanding)
-        )
-        self.horizontalLayout.addLayout(self.leftVerticalLayout)
+        # Макет колонки формы
+        form_frame = QtWidgets.QFrame(window)
+        form_frame.setObjectName("form_frame")
+        form_frame.setMaximumWidth(350)
 
-        self.mainVerticalLayout = QtWidgets.QVBoxLayout()
-        self.mainVerticalLayout.setObjectName("mainVerticalLayout")
-        self.mainVerticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.mainVerticalLayout.addItem(
+        self.form_column = QtWidgets.QVBoxLayout(form_frame)
+        self.form_column.setObjectName("form_column")
+        self.form_column.setContentsMargins(40, 40, 40, 40)
+        self.form_column.addItem(
             QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
         )
-        self.horizontalLayout.addLayout(self.mainVerticalLayout)
-        self.secondVerticalLayout = QtWidgets.QVBoxLayout()
-        self.secondVerticalLayout.setObjectName("secondVerticalLayout")
-        self.secondVerticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.addLayout(self.secondVerticalLayout)
-        self.secondVerticalLayout.addItem(
-            QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Policy.Expanding,
-                                  QtWidgets.QSizePolicy.Policy.Expanding)
-        )
-        window.setLayout(self.horizontalLayout)
+        horizontal_layout.addWidget(form_frame)
 
         # Макет для формы авторизации
-        self.authBox = QtWidgets.QGroupBox(window)
-        self.authBox.setObjectName("authBox")
-        self.authBox.setMaximumSize(QtCore.QSize(350, 350))
-        self.authBox.setContentsMargins(5, 5, 5, 5)
-        self.authBox.setStyleSheet("""
-            QGroupBox {
-                background-color: #eef2db;
-                border: 1px solid #eef2db;
-                border-radius: 10px;
+        form_box = QtWidgets.QGroupBox(window)
+        form_box.setObjectName("form_box")
+        form_box.setMaximumSize(QtCore.QSize(350, 350))
+        form_box.setContentsMargins(5, 5, 5, 5)
+        form_box.setStyleSheet("""
+            QGroupBox#form_box {
+                background-color: transparent;
             }
         """)
-        self.authBox.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=10,
-            color=QtGui.QColor("#c0cfaf"),
-            offset=QtCore.QPointF(0, 0)
-        ))
-        self.authBoxLayout = QtWidgets.QVBoxLayout()
-        self.authBoxLayout.setObjectName("authBoxLayout")
-        self.authBoxLayout.setContentsMargins(5, 5, 5, 5)
-        self.authBox.setLayout(self.authBoxLayout)
-        self.mainVerticalLayout.addWidget(self.authBox)
+        form_box_layout = QtWidgets.QVBoxLayout()
+        form_box_layout.setObjectName("form_box_layout")
+        form_box_layout.setContentsMargins(5, 5, 5, 5)
+        form_box.setLayout(form_box_layout)
+        self.form_column.addWidget(form_box)
+        self.form_column.addItem(
+            QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
+        )
 
         # Заголовок формы авторизации
-        self.authTitleLabel = QtWidgets.QLabel(window)
-        self.authTitleLabel.setObjectName("authTitleLabel")
-        self.authTitleLabel.setText("Авторизация")
-        self.authTitleLabel.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.authTitleLabel.setStyleSheet("""
-            QLabel {
-                font-size: 24px;
-            }
-        """)
-        self.authTitleLabel.setContentsMargins(5, 5, 5, 10)
-        self.authBoxLayout.addItem(QtWidgets.QSpacerItem(
+        self.title_label = widgets_factory.heading2()
+        self.title_label.setObjectName("title_label")
+        self.title_label.setText("Аутентификация")
+        self.title_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.title_label.setContentsMargins(5, 5, 5, 10)
+        form_box_layout.addItem(QtWidgets.QSpacerItem(
             10, 10, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.MinimumExpanding
         ))
-        self.authBoxLayout.addWidget(self.authTitleLabel)
+        form_box_layout.addWidget(self.title_label)
 
         # Ответ сервера
-        self.authResponseLabel = QtWidgets.QLabel(window)
-        self.authResponseLabel.setObjectName("authResponseLabel")
-        self.authResponseLabel.setWordWrap(True)
-        self.authResponseLabel.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.authResponseLabel.setStyleSheet("""
+        self.response_label = QtWidgets.QLabel(window)
+        self.response_label.setObjectName("response_label")
+        self.response_label.setWordWrap(True)
+        self.response_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.response_label.setStyleSheet("""
             QLabel {
                 font-size: 12px;
                 font-weight: bold;
                 color: red;
             }
         """)
-        self.authResponseLabel.hide()
-        self.authBoxLayout.addWidget(self.authResponseLabel)
+        self.response_label.hide()
+        form_box_layout.addWidget(self.response_label)
 
-        # Форма авторизации
-        self.authFormHorizontalLayout = QtWidgets.QHBoxLayout()
-        self.authFormHorizontalLayout.setObjectName("authFormHorizontalLayout")
-        self.authFormHorizontalLayout.setContentsMargins(0, 0, 0, 0)
-
-        self.authFormBox = QtWidgets.QGroupBox(self.authBox)
-        self.authFormBox.setObjectName("authFormBox")
-        self.authFormBox.setMaximumSize(QtCore.QSize(300, 400))
-        self.authFormBoxLayout = QtWidgets.QVBoxLayout()
-        self.authFormBoxLayout.setObjectName("authFormBoxLayout")
-        self.authFormBoxLayout.setContentsMargins(10, 10, 10, 0)
-        self.authFormBoxLayout.setProperty("spacing", 12)
-        self.authFormBox.setLayout(self.authFormBoxLayout)
-
-        self.authFormHorizontalLayout.addWidget(self.authFormBox)
-        self.authBoxLayout.addLayout(self.authFormHorizontalLayout)
+        # Форма полей ввода
+        form_layout = QtWidgets.QFormLayout()
+        form_layout.setObjectName("authFormHorizontalLayout")
+        form_layout.setContentsMargins(10, 10, 10, 0)
+        form_layout.setSpacing(12)
+        form_box_layout.addLayout(form_layout)
 
         # Поля
-        self.login_line_edit = QtWidgets.QLineEdit()
-        self.login_line_edit.setObjectName("loginLineEdit")
-        self.login_line_edit.setPlaceholderText("Логин")
-        self.login_line_edit.setStyleSheet("""
-            QLineEdit {
-                border: 1px solid #fff;
-                border-radius: 5px;
-                padding: 5px;
-                color:  #1a3f13;
-                font-weight: bold;
-                font-size: 14px;
-            }        
-        """)
-        self.login_line_edit.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=5,
-            color=QtGui.QColor("#949a5e"),
-            offset=QtCore.QPointF(0, 0)
-        ))
+        self.login_line_edit = widgets_factory.line_edit()
+        self.login_line_edit.setPlaceholderText("Имя пользователя")
 
-        self.password_line_edit = QtWidgets.QLineEdit()
+        self.password_line_edit = widgets_factory.line_edit()
         self.password_line_edit.setObjectName("passwordLineEdit")
         self.password_line_edit.setPlaceholderText("Пароль")
-        self.password_line_edit.setMaxLength(32)
-        self.password_line_edit.setStyleSheet("""
-            QLineEdit {
-                border: 1px solid #fff;
-                border-radius: 5px;
-                padding: 5px;
-                color:  #1a3f13;
-                font-weight: bold;
-                font-size: 14px;
-            }        
-        """)
         self.password_line_edit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
-        self.password_line_edit.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=5,
-            color=QtGui.QColor("#949a5e"),
-            offset=QtCore.QPointF(0, 0)
-        ))
 
-        self.repeat_password_line_edit = QtWidgets.QLineEdit()
+        self.repeat_password_line_edit = widgets_factory.line_edit()
         self.repeat_password_line_edit.setObjectName("repeatPasswordLineEdit")
         self.repeat_password_line_edit.setPlaceholderText("Повторите пароль")
-        self.repeat_password_line_edit.setMaxLength(32)
-        self.repeat_password_line_edit.setStyleSheet("""
-            QLineEdit {
-                border: 1px solid #fff;
-                border-radius: 5px;
-                padding: 5px;
-                color:  #1a3f13;
-                font-weight: bold;
-                font-size: 14px;
-            }        
-        """)
         self.repeat_password_line_edit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
-        self.repeat_password_line_edit.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=5,
-            color=QtGui.QColor("#949a5e"),
-            offset=QtCore.QPointF(0, 0)
-        ))
 
-        self.signup_button = QtWidgets.QPushButton()
-        self.signup_button.setObjectName("signupButton")
+        self.signup_button = widgets_factory.primary_button()
         self.signup_button.setText("Зарегистрироваться")
-        self.signup_button.setStyleSheet("""
-            QPushButton {
-                background-color: #729037;
-                color: #dce29f;
-                font-weight: bold;
-                font-size: 16px;
-       
-                border-radius: 5px;
-                padding: 5px;
-                margin-top: 5px;
-            }
-            
-            QPushButton:hover {
-                background-color: #9ab740;
-                color: #fff;
-            }
-            
-            QPushButton:pressed {
-                background-color: #729037;
-                color: #dce29f;
-            }
-            
-        """)
-        self.signup_button.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=10,
-            color=QtGui.QColor("#93a63e"),
-            offset=QtCore.QPointF(0, 0)
-        ))
 
-        self.signin_button = QtWidgets.QPushButton()
+        self.signin_button = widgets_factory.primary_button()
         self.signin_button.setObjectName("signinButton")
-        self.signin_button.setText("Авторизоваться")
-        self.signin_button.setStyleSheet("""
-            QPushButton {
-                background-color: #729037;
-                color: #dce29f;
-                font-weight: bold;
-                font-size: 16px;
-       
-                border-radius: 5px;
-                padding: 5px;
-                margin-top: 5px;
-            }
-            
-            QPushButton:hover {
-                background-color: #9ab740;
-                color: #fff;
-            }
-            
-            QPushButton:pressed {
-                background-color: #729037;
-                color: #dce29f;
-            }
-            
-        """)
-        self.signin_button.setGraphicsEffect(QGraphicsDropShadowEffect(
-            blurRadius=10,
-            color=QtGui.QColor("#93a63e"),
-            offset=QtCore.QPointF(0, 0)
-        ))
-        self.authFormBoxLayout.addWidget(self.login_line_edit)
-        self.authFormBoxLayout.addWidget(self.password_line_edit)
-        self.authFormBoxLayout.addWidget(self.repeat_password_line_edit)
-        self.authFormBoxLayout.addWidget(self.signup_button)
-        self.authFormBoxLayout.addWidget(self.signin_button)
+        self.signin_button.setText("Вход")
 
-        # Ссылки
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.switchAuthStateLabel = QtWidgets.QLabel(self.authBox)
-        self.switchAuthStateLabel.setObjectName("switchAuthStateLabel")
-        self.switchAuthStateLabel.setText("Регистрация")
-        self.switchAuthStateLabel.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.switchAuthStateLabel.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
-        self.switchAuthStateLabel.setStyleSheet("""
-            QLabel {
-                color: #a4b096;
-                text-decoration: underline;
-                font-size: 15px;
-            }
-        """)
-        self.horizontalLayout_2.addWidget(self.switchAuthStateLabel)
-        self.authBoxLayout.addItem(self.horizontalLayout_2)
-
-        self.authBoxLayout.addItem(
+        form_layout.addWidget(self.login_line_edit)
+        form_layout.addWidget(self.password_line_edit)
+        form_layout.addWidget(self.repeat_password_line_edit)
+        form_layout.addWidget(self.signup_button)
+        form_layout.addWidget(self.signin_button)
+        form_box_layout.addItem(
             QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
         )
 
-        self.mainVerticalLayout.addItem(
-            QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
+        # Poster panel
+        self.poster_panel = QtWidgets.QFrame()
+        self.poster_panel.setObjectName("poster_panel")
+        self.poster_panel.setStyleSheet("""
+            QFrame#poster_panel {
+                background-color: $BG3;
+                border: none;
+            }
+        """.replace(
+            "$BG3", widgets_factory.theme.third_background
+        ))
+        horizontal_layout.addWidget(self.poster_panel)
+
+        # Poster Content
+        poster_layout = QtWidgets.QVBoxLayout(self.poster_panel)
+        poster_layout.setObjectName("poster_layout")
+        poster_layout.setContentsMargins(50, 0, 50, 0)
+        poster_layout.setSpacing(30)
+        poster_layout.addStretch(1)
+
+        poster_header = widgets_factory.heading1(
+            "...",
+            widgets_factory.theme.text_tertiary
         )
+        poster_header.setObjectName("poster_header")
+        poster_header.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.poster_header = poster_header
+        poster_layout.addWidget(poster_header)
+
+        poster_text_layout = QtWidgets.QHBoxLayout()
+        poster_text = widgets_factory.label("...", widgets_factory.theme.text_tertiary)
+        poster_text.setObjectName("poster_text")
+        poster_text.setWordWrap(True)
+        poster_text.setMaximumWidth(350)
+        poster_text.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.poster_text = poster_text
+        poster_text_layout.addWidget(poster_text)
+        poster_layout.addLayout(poster_text_layout)
+
+        button_layout = QtWidgets.QHBoxLayout()
+        poster_button = widgets_factory.outline_button()
+        poster_button.setObjectName("poster_button")
+        poster_button.setMaximumWidth(100)
+        poster_button.setText("...")
+        button_layout.addWidget(poster_button)
+        self.poster_button = poster_button
+        poster_layout.addLayout(button_layout)
+
+        poster_layout.addStretch(1)
+
+
 
         self.retranslateUi(window)
         QtCore.QMetaObject.connectSlotsByName(window)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Login"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Auth"))
